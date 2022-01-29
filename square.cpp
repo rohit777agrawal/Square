7b3e49a6fc8f589a807542e9874f5ac7f4632f21
rohit777agrawal <rohit777agrawal@gmail.com>
2022-01-29 11:47:07 -0800

    first commit

---
+++ square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  while (i < 10){
+    std::cout << i << " ";
+    std::cout << i*i << "\n";
+
+    i++;
+  }
+}
